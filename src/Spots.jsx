1a87bec00f6b2a73734aded92da06d096677eaae
felix <felixg10@nycstudents.net>
2023-09-19 10:38:47 -0400

    using public images for spots

--- src/Spots.jsx
+++ src/Spots.jsx
@@ -3,10 +3,10 @@ import React from 'react'
 const Spots = () => {
   return (
     <span >
-      <img src="https://cdn.discordapp.com/attachments/798194521321504788/1153516943165370388/image0.jpg" height='300px' alt="rooftop field" />
+      <img src="https://www.rogersarchitects.com/assets/image-cache/filesystem/images/project_images/Elevated%20Acre%20at%2055%20Water%20Street/Elevated-Acre-NYC-Water-Street-05.844d8897.jpg" height='300px' alt="rooftop field" />
       <caption><a href="https://maps.app.goo.gl/1koT6X1MWqy7G64g6">Elevated Acres near FiDi</a> is a nice place to relax in</caption>
       <br />
-      <img src="https://cdn.discordapp.com/attachments/798194521321504788/1153520271421096006/image0.jpg" height='300px' alt="a sunset from the boardwalk" />
+      <img src="https://images.unsplash.com/photo-1568032278263-336fea037e17?ixid=MnwxMzcxOTN8MHwxfHNlYXJjaHw5fHxjb25leSUyMGlzbGFuZHxlbnwwfHx8fDE2NTkzNjM3Njc&ixlib=rb-1.2.1&fm=jpg&w=3652&h=2594&fit=max" height='300px' alt="a sunset from the boardwalk" />
       <caption>you can catch some great sunsets from <a href="https://maps.app.goo.gl/K1cSRumNNHcEbbr9A">the boardwalk</a> in coney island</caption>
     </span>    
   )
